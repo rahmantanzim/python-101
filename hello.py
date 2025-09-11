c2cc32ec33321fa259d0d05c9883f0ea4f2057ae
Tanzim <tanzimr@mun.ca>
2025-09-11 03:13:56 -0230

    Python Syntax

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+# Indentation 
+if 5 > 2:
+    print("five is bigger than two")
+
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+
+if 5>3:
+    print('5 is bigger')
+    print('3 is smaller')
