29c38c0225b50155f81615d06c4db332d8565930
Tanzim <tanzimr@mun.ca>
2025-09-13 01:10:17 -0230

    Python variables

--- hello.py
+++ hello.py
@@ -21,3 +21,48 @@ print(department)
 """ this is a comment
 written in
 more than 1 line """
+
+# In python, if you wanna create a global variable inside a fuinction, you can use the 'global' keyword:
+
+x = 'Awesome'
+
+def myFunc():
+    global x
+    x = 'Sexy'
+
+print("Python is " + x)
+
+myFunc()
+
+print("Python is " + x)
+
+
+x = 5
+y = "John"
+print(5 , y) 
+
+x = "Five"
+
+x = str(5)
+print(x)
+print(type(x))
+X = 5
+print(x, type(X))
+# String can be created either by single or double quote
+
+
+#variabel name can start with underscore or letter not a number
+# Varibale name 
+
+x,y,z = 'Mango','Banana','Cherry'
+print(z)
+
+a =  "Student"
+print("All of them are "+ a)
+#In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+
+x = str(30)
+y = "Thirty"
+print("Thirtys " + x  )
+
+# for printing something with the plus operator, both side of + has to be in same type
