4a90cf5e30819fae95bbca3cefcd34e0d9a7492e
Tanzim <tanzimr@mun.ca>
2025-10-08 20:05:00 -0230

    added return value, success in output

--- assignmetn01_11.py
+++ assignmetn01_11.py
@@ -8,9 +8,14 @@ from typing import List, Dict, Union
 
 
 def create_data_map(keys: List[str], values: List[Union[str,int,float]]) -> Dict[str, Union[str,int,float]]:
-    return 0
+    # arguments:
+    # keys:  List[str] -> a list of string type
+    # values: a list of values corresponding to each key
+    return dict(zip(keys,values))
 
 str_keys = ['id','name', 'age', 'height']
 vals = [1,'Tanzim', 33, 5.80]
 
 result = create_data_map(str_keys, vals)
+
+print(result) # Outputs {'id': 1, 'name': 'Tanzim', 'age': 33, 'height': 5.8}
