2b3c291e736f0ea3835cb0ee5ae41da69b46fe6c
Tanzim <tanzimr@mun.ca>
2025-10-08 19:56:34 -0230

    imported from typing import List, Dict, Union and added function parameter

--- assignmetn01_11.py
+++ assignmetn01_11.py
@@ -4,6 +4,13 @@
 # a List of values. 
 # The function must return a Dictionary where the elements of the first list are mapped to the corresponding elements of the second list. Include Type Hints for all arguments (using List, Dict, and Union from the typing module, as appropriate) and the return value. 
 # Assume the input lists have the same length.
+from typing import List, Dict, Union
 
-def create_data_map():
+
+def create_data_map(keys: List[str], values: List[Union[str,int,float]]) -> Dict[str, Union[str,int,float]]:
     return 0
+
+str_keys = ['id','name', 'age', 'height']
+vals = [1,'Tanzim', 33, 5.80]
+
+result = create_data_map(str_keys, vals)
