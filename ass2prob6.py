d20326d5b6c6a218afd1622ba2ee22380968fceb
Tanzim <tanzimr@mun.ca>
2025-11-14 20:51:59 -0330

    completed with the timing function

--- ass2prob6.py
+++ ass2prob6.py
@@ -28,7 +28,13 @@ def with_list_comprehension(total_chunks=1000, chunk_size=100):
         for i in range(total_chunks)
         for j in range(chunk_size)
     ]
-def get_time():
-    pass
+def get_time(fn,*args,repeats = 5, **kwargs):
+    best_time = float('inf')
+    for _ in range(repeats):
+        start = time.perf_counter()
+        fn(*args, **kwargs)
+        end = time.perf_counter()
+        best_time = min(best_time, end - start)
+    return best_time
 def benchmark():
     pass
