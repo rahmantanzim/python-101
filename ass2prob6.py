60721d4c50f9006d3eae41c4166539dec9199730
Tanzim <tanzimr@mun.ca>
2025-11-14 20:38:22 -0330

    declare all function definitions

--- ass2prob6.py
+++ ass2prob6.py
@@ -1,1 +1,14 @@
 import time
+
+def with_concatenation():
+    pass
+
+def with_extension():
+    pass
+
+def with_list_comprehension():
+    pass
+def get_time():
+    pass
+def benchmark():
+    pass
