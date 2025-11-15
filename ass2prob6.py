3b391b3443e0bb3a01204897dd98a08c130ac9f4
Tanzim <tanzimr@mun.ca>
2025-11-14 20:48:27 -0330

    completed with concat,extension and comprehension function

--- ass2prob6.py
+++ ass2prob6.py
@@ -2,9 +2,10 @@ import time
 
 #helper function
 def create_chunk(chunk_size):
+    result = []
     return list(range(chunk_size))
 
-def make_empty():
+def create_empty():
     return [] 
     
 def with_concatenation(total_chunks = 1000, chunk_size = 100):
@@ -14,11 +15,19 @@ def with_concatenation(total_chunks = 1000, chunk_size = 100):
         result+= chunk
     return result
 
-def with_extension():
-    pass
+def with_extension(total_chunks=1000, chunk_size=100):
+    chunk = create_chunk(chunk_size)
+    result = create_empty() 
+    for _ in range(total_chunks):
+        result.extend(chunk)
+    return result
 
-def with_list_comprehension():
-    pass
+def with_list_comprehension(total_chunks=1000, chunk_size=100):
+    return [
+        i * chunk_size + j
+        for i in range(total_chunks)
+        for j in range(chunk_size)
+    ]
 def get_time():
     pass
 def benchmark():
