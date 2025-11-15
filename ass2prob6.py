afded9281cbee4bda25b4fe4825061c4e886bc9a
Tanzim <tanzimr@mun.ca>
2025-11-14 20:44:37 -0330

    created a helper function

--- ass2prob6.py
+++ ass2prob6.py
@@ -1,7 +1,18 @@
 import time
 
-def with_concatenation():
-    pass
+#helper function
+def create_chunk(chunk_size):
+    return list(range(chunk_size))
+
+def make_empty():
+    return [] 
+    
+def with_concatenation(total_chunks = 1000, chunk_size = 100):
+    chunk = list(range(chunk_size))
+    result = []
+    for _ in range(total_chunks):
+        result+= chunk
+    return result
 
 def with_extension():
     pass
